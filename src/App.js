6049a0fb9f5cdb91c486ce1798cbbc726bcb1298
volodpj <volod.af@gmail.com>
2019-09-08 21:48:38 +0300

    satrt

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import './reset.css';
+
+function App() {
+  return (
+    <div className="wraper">
+      <h1>helo my name volodymyr</h1>
+    </div>
+  );
+}
+
+export default App;
